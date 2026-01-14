f0ef812868e18ebd495f759069014dee5c2e676f
Kevin Stewart-Mercurio <kevinstewartmercurio@gmail.com>
2026-01-14 16:30:29 -0500

    init

---
+++ index.ts
@@ -0,0 +1,43 @@
+import { chromium } from "playwright";
+
+async function readBoard(page) {
+  return await page.$$eval("div.square", (squares) => {
+    return squares.map((sq) => {
+      const classes = sq.className.split(" ");
+      return {
+        id: sq.id,
+        isOpen: classes.some((c) => c.startsWith("open")),
+        number: classes.find((c) => c.startsWith("open"))
+          ? parseInt(classes.find((c) => c.startsWith("open")).replace("open", ""))
+          : null,
+        isFlagged: classes.includes("bombflagged")
+      };
+    })
+  })
+}
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false, // shows the browser window
+    slowMo: 50,      // optional: slows actions so you can see them
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://minesweeperonline.com/', {
+    waitUntil: 'domcontentloaded',
+  });
+
+  await page.click("#display-link");
+  await page.click("#zoom150");
+  await page.click("#nightMode");
+  await page.click("#display-link");
+
+  await page.click("#\\38_15");
+
+  const board = await readBoard(page);
+  board.forEach((sq) => {
+    if (sq.isOpen) {
+      console.log(sq)
+    }
+  })
+})();
