e1e002d85e4ea831f10738860ce82f242c5d6681
Kevin Stewart-Mercurio <kevinstewartmercurio@gmail.com>
2026-01-15 10:53:27 -0500

    feat: adding applyFlags logic

--- index.ts
+++ index.ts
@@ -1,52 +1,95 @@
 import { chromium } from "playwright";
 
+let headless = true;
+
+for (const arg of Bun.argv) {
+  if (arg.startsWith("--headless=")) {
+    const val = arg.split("=")[1].toLowerCase();
+    headless = val === "true";
+  }
+}
+
 type Board = {
   id: string;
   isOpen: boolean;
   number: number | null;
   isFlagged: boolean;
-}[]
+}[];
 
 async function readBoard(page) {
-  return await page.$$eval('div.square:not([style*="display: none"])', (squares) => {
-    return squares.map((sq) => {
-      const classes = sq.className.split(" ");
-      return {
-        id: sq.id,
-        isOpen: classes.some((c) => c.startsWith("open")),
-        number: classes.find((c) => c.startsWith("open"))
-          ? parseInt(classes.find((c) => c.startsWith("open")).replace("open", ""))
-          : null,
-        isFlagged: classes.includes("bombflagged")
-      };
-    })
-  })
+  return await page.$$eval(
+    'div.square:not([style*="display: none"])',
+    (squares) => {
+      return squares.map((sq) => {
+        const classes = sq.className.split(" ");
+        return {
+          id: sq.id,
+          isOpen: classes.some((c) => c.startsWith("open")),
+          number: classes.find((c) => c.startsWith("open"))
+            ? parseInt(
+                classes.find((c) => c.startsWith("open")).replace("open", ""),
+              )
+            : null,
+          isFlagged: classes.includes("bombflagged"),
+        };
+      });
+    },
+  );
 }
 
-function applyFlags(board: Board) {
+async function applyFlags(board: Board, page) {
+  const bombIdsForFlagging = new Set();
+
   board.forEach((sq) => {
     if (sq.isOpen && sq.number) {
-      const [x, y] = sq.id.split("_").map(nStr => parseInt(nStr))
+      const [x, y] = sq.id.split("_").map((nStr) => parseInt(nStr));
       let count = 0;
+      let neighborIds = [];
 
-      if (x - 1 && y - 1 && !board[(x - 2) * 30 + y - 2].isOpen && !board[(x - 2) * 30 + y - 2].isFlagged) {
-        count++;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (
+            x + i &&
+            x + i <= 16 &&
+            y + j &&
+            y + j <= 30 &&
+            !board[(x - 1 + i) * 30 + y - 1 + j].isOpen
+          ) {
+            count++;
+            neighborIds.push(board[(x - 1 + i) * 30 + y - 1 + j].id);
+          }
+        }
       }
 
-      console.log(x, y, count);
+      if (sq.number === count) {
+        neighborIds.forEach((id) => {
+          bombIdsForFlagging.add(id);
+
+          const [bombX, bombY] = id.split("_").map((nStr) => parseInt(nStr));
+          board[(bombX - 1) * 30 + bombY - 1].isFlagged = true;
+        });
+      }
     }
-  })
+  });
+
+  for (const id of bombIdsForFlagging) {
+    const sq = page.locator(`div.square[id="${id}"]:not(.bombflagged)`);
+
+    if ((await sq.count()) > 0) {
+      await sq.click({ button: "right" });
+    }
+  }
 }
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false,
-    slowMo: 50,
+    headless: headless,
+    slowMo: 200,
   });
 
   const page = await browser.newPage();
-  await page.goto('https://minesweeperonline.com/', {
-    waitUntil: 'domcontentloaded',
+  await page.goto("https://minesweeperonline.com/", {
+    waitUntil: "domcontentloaded",
   });
 
   await page.click("#display-link");
@@ -57,5 +100,5 @@ function applyFlags(board: Board) {
   await page.click("#\\38_15");
 
   const board = await readBoard(page);
-  applyFlags(board);
+  applyFlags(board, page);
 })();
